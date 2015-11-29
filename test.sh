d4929458b1bbfee1388c52e56000cef7b7ef0e1e
Tom Lee <thomas.j.lee@gmail.com>
2015-11-29 18:56:18 -0500

    fixed test script

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 set -eu
 
 GITSHA="$(git log -n 1 | head -n 1 | sed 's/commit //' | head -c 8)"
-./run.sh '/Volumes/mapbox/cat photos/122MEDIA/' $GITSHA &
-PID=$!
+./run.sh '/Volumes/mapbox/cat photos/123MEDIA/' $GITSHA &
+sleep 2
+PID="$(ps aux | grep node | grep -v grep | awk '{print $2}')"
 echo "PID = $PID"
+if [ -z "$PID" ]; then
+    echo "process did not launch"
+    exit 1
+fi
+
 i=0
 mkdir -p out/$GITSHA/test
 while [ $i -lt 120 ]; do
