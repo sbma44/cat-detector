ddb246425159be9f0d02833fb830a6d1fddb48ae
Tom Lee <thomas.j.lee@gmail.com>
2015-11-29 20:36:24 -0500

    satisfy set -eu

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@ set -eu
 
 SRC="$1"
 GITSHA="$(git log -n 1 | head -n 1 | sed 's/commit //' | head -c 8)"
-if [ -z "$2" ]; then
-    DEST="out/${2:-$GITSHA}"
+PARAM2=${2:-""}
+if [ -z "$PARAM2" ]; then
+    DEST="out/${PARAM2:-$GITSHA}"
 else
-    DEST="$2"
+    DEST="$PARAM2"
 fi
 mkdir -p $DEST/img
 node index.js "$SRC" "$DEST" | tee $DEST/$(basename $1).log
