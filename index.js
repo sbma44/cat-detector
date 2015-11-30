c9b37f0063f7d89b1db0f27728f591fe9834a7d3
Tom Lee <thomas.j.lee@gmail.com>
2015-11-29 20:35:25 -0500

    bugfix

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (require.main === module) {
             var numDiff = pixelmatch(fileCache[0], fileCache[1], diff.data, width, height, {threshold: 0.1, includeAA: false});
             // var numDiff = pixelmatch(fileCache[0], fileCache[1], null, width, height, {threshold: 0.1, includeAA: false});
             if (numDiff > 200) {
-                fs.writeFileSync(path.normalize(process.argv[3] + '/diff_' + path.basename(f2, '.jpg') + '.png'), diff);
+                fs.writeFileSync(path.normalize(process.argv[3] + '/img/diff_' + path.basename(f2, '.jpg') + '.png'), diff);
                 // diff.pack().pipe(fs.createWriteStream(path.normalize(process.argv[3] + '/diff_' + path.basename(f2, '.jpg') + '.png')));
                 console.log(f2);
             }
--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@ set -eu
 
 SRC="$1"
 GITSHA="$(git log -n 1 | head -n 1 | sed 's/commit //' | head -c 8)"
-DEST=${2:-$GITSHA}
-mkdir -p out/$DEST/img
-node index.js "$SRC" "$DEST" | tee out/$DEST/$(basename $1).log
+if [ -z "$2" ]; then
+    DEST="out/${2:-$GITSHA}"
+else
+    DEST="$2"
+fi
+mkdir -p $DEST/img
+node index.js "$SRC" "$DEST" | tee $DEST/$(basename $1).log
